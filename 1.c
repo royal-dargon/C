93cc05e3db92c2da73107619d5650b5e0d103abd
royal-dargon <72372815+royal-dargon@users.noreply.github.com>
2021-04-14 01:07:19 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,54 +1,80 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int power(int f ,int r)
+#include<stdio.h>
+
+// 最初的冒泡排序的模型
+void SortOne()
 {
-    int t,result = f;
-
-
-    for (t = 1; t<r; t++) {
-        result *= 10;
-    }
-        return result;
-
-} //处理几次方 
-
-int MyAtoi(const char* c)
-{
-    int m[100];
-	int n,i,sum = 0,temp;
-    int r = 0 ,flag = 1;//定义flag处理负数 
-    while (*c == ' ')
+    int num[100];
+    int n,i;
+    int j,temp;
+    int count=0;
+    printf("please put a number about number of array\n");
+    scanf("%d",&n);
+    for(i = 0;i < n;i ++)
     {
-    	c++;
-	}  //处理空格 
-    while ((*c>='0'&&*c<='9')||*c=='-')
+        scanf("%d",&num[i]);
+    }
+    // 定义n-1趟轮次
+    for(i = 1;i < n ;i ++)
     {
-    	if (*c == '-')
-    	{
-    		flag = -1;
-    		c++;
-		}
-        m[r] = *(c++)-48 ;
-        r++;
+        for(j = 0;j < n - 1;j ++)
+        {
+            if(num[j] > num[j+1])
+            {
+                temp = num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+        }
+        count ++;
     }
-    temp = 1;
-    for (i = r-1; i >= 0;i--){
-        sum += power(m[i],temp);
-        temp++;
+    for(i = 0;i < n;i ++)
+    {
+        printf("%d\t",num[i]);
+        //
     }
-    return flag*sum;
+    printf("%d\n",count);
 }
-int main()
+
+// 优化版的冒泡排序
+void SortTwo()
 {
-    char *str;
-    char j[100];   
-    int n,inter;
-    printf("请输入一个字符串:");
-    scanf("%s",j);
-    str = j;   //注意类型相同 
-    n = MyAtoi(str);
-    inter = atoi(str);
-    printf("%d %d",n,inter);
-    return 0;
+   int numb[100];
+    int n1,k;
+    int w,temp1;
+    int count1=0;
+    printf("please put a number about number of array\n");
+    scanf("%d",&n1);
+    for(k = 0;k < n1;k ++)
+    {
+        scanf("%d",&numb[k]);
+    }
+    for(k = 0;k < n1-1;k++)
+    {
+        int flag1 = 1; //定义一个flag作为标记
+        for(w = 0;w < n1-1;w ++)
+        {
+            if(numb[w] > numb[w+1])
+            {
+                temp1 = numb[w];
+                numb[w] = numb[w+1];
+                numb[w+1] = temp1;
+                flag1 = 0;
+            }
+        }
+        count1 ++;
+        if (flag1 == 1)
+            break;
+    }
+    for(k = 0;k < n1;k ++)
+    {
+        printf("%d\t",numb[k]);
+    }
+    printf("%d\n",count1);
+}
+
+void main()
+{
+    SortOne();
+    //return 0;
+    SortTwo();
 }
